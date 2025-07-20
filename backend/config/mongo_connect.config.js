6fd1a5be793d16fe911081d9978c7cb0776a2ec6
Daniel Ruz <65560614+Glon8@users.noreply.github.com>
2025-07-20 20:59:05 +0300

    Update mongo_connect.config.js

--- backend/config/mongo_connect.config.js
+++ backend/config/mongo_connect.config.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connect_mongo = async () => {
     try{
-        const conn = await mongoose.connect('mongodb+srv://daniruzanov:Y6sC9w3KytnR1rWO@cluster0.ojmevss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        const conn = await mongoose.connect('mongo url');
 
         console.log(`Mongo Data Base connected: ${ conn.connection.host }`)
     }
