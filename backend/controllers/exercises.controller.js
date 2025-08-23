ab22dd9a94adf2840b134981cad658fcbd374ebc
Daniel Ruz <65560614+Glon8@users.noreply.github.com>
2025-08-23 13:36:39 +0300

    Power & Roots theme added + Basic exam

--- backend/controllers/exercises.controller.js
+++ backend/controllers/exercises.controller.js
@@ -50,7 +50,7 @@ const sumAndSub = async (dif, sett) => {
 
             exercise.push({
                 type: typo, message: (typo === 0 ? (`${first} - ${second} = ?`) :
-                    (String.raw`\begin{array}{r} - ${first} \\ \underline{${second}} \\ \end{array}`))
+                    (String.raw`\begin{array}{r} -\;${first} \\ \underline{${second}} \\ \end{array}`))
             });
 
             answer.push(first - second);
@@ -61,7 +61,7 @@ const sumAndSub = async (dif, sett) => {
 
             exercise.push({
                 type: typo, message: (typo === 0 ? (`${first} + ${second} = ?`) :
-                    (String.raw`\begin{array}{r} + ${first} \\ \underline{${second}} \\ \end{array}`))
+                    (String.raw`\begin{array}{r} +\;${first} \\ \underline{${second}} \\ \end{array}`))
             });
 
             answer.push(first + second);
@@ -158,7 +158,7 @@ const multiplyAndDivide = async (dif, sett) => {
 
             exercise.push({
                 type: typo, message: (typo == 0 ? (`${product} : ${first} = ?`) :
-                    (String.raw`\begin{array}{r} : ${product} \\ \underline{${first}} \\ \end{array}`))
+                    (String.raw`\begin{array}{r} :\;${product} \\ \underline{${first}} \\ \end{array}`))
             });
             answer.push(second);
             break;
@@ -168,7 +168,7 @@ const multiplyAndDivide = async (dif, sett) => {
 
             exercise.push({
                 type: typo, message: (typo == 0 ? (`${first} * ${second} = ?`) :
-                    (String.raw`\begin{array}{r} * ${first} \\ \underline{${second}} \\ \end{array}`))
+                    (String.raw`\begin{array}{r} *\;${first} \\ \underline{${second}} \\ \end{array}`))
             });
             answer.push(first * second);
             break;
@@ -658,6 +658,295 @@ const fractionsTest = async (sett) => {
     return pack;
 }
 
+const powerAndRoot = async (dif, sett) => {
+    const box = {};
+
+    const turn = Math.floor(Math.random() * 2); // turn ll determine whom ll carry the root and power (0 - first, 1 - second)
+    const main = Math.floor(Math.random() * 3); // determine if there ll be a main root or power carrier (0 - no main, 1 - main root carrier, 2 - main power carrier)
+    const sign = Math.floor(Math.random() * 4); // sign (0 - minus, 1 - plus, 2 - divide, 3 - multiply)
+    const powerFlip = Math.floor(Math.random() * 2); // determine if the digits carry root or power (0 - root, 1 - power)
+
+    let quota; // the limit that Ill generate powers from/ possible roots
+    let first;
+    let second;
+    let carrier;
+    let rawPow;
+    let mainPow;
+    let mp;
+    // rawPow / mainPow ll be used:
+    // >> rawPower ** 2 = A
+    // >> sqrt(A)
+
+    if (dif < 40) quota = 16;
+    else if (dif >= 40) quota = 36;
+
+    const exercise = [];
+    const answer = [];
+
+    if (sign == 0) { // minus sign
+        mp = Math.floor(Math.random() * ((quota - 1) - Math.floor(quota * 0.4))) + Math.floor(quota * 0.4);
+        mainPow = !main ? 0 : (main == 1 ? (mp ** 2) : (mp));
+        rawPow = !main ?
+            (Math.floor(Math.random() * (quota - Math.floor(quota * 0.1) - 1)) + Math.floor(quota * 0.1) + 1) :
+            (!turn ?
+                (Math.floor(Math.random() * ((!powerFlip ? (quota ** 2) : (quota)) - mainPow)) + mainPow) :
+                (Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1)));
+        first = !turn ?
+            (!powerFlip ? (rawPow ** 2) : (rawPow)) :
+            (!main ?
+                ((Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1)) + (!powerFlip ? (rawPow) : (rawPow ** 2))) :
+                (mainPow + (!powerFlip ? (rawPow) : (rawPow ** 2))));
+        second = !!turn ?
+            (!powerFlip ? (rawPow ** 2) : (rawPow)) :
+            (!main ?
+                (Math.floor(Math.random() * ((!powerFlip ? (rawPow) : (rawPow ** 2)) - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1)) :
+                ((!powerFlip ? (rawPow) : (rawPow ** 2)) - mainPow));
+
+        let exerciseStr;
+        let answerStr;
+
+        if (!main) {
+            answerStr = !turn ?
+                (!powerFlip ?
+                    (rawPow - second) :
+                    (rawPow ** 2 - second)) :
+                (!powerFlip ?
+                    (first - rawPow) :
+                    (first - rawPow ** 2));
+
+            exerciseStr = !turn ?
+                (!powerFlip ?
+                    (`\\sqrt{${first}}\\;-\\;${second}\\;=\\;?`) :
+                    (`${first}^{2}\\;-\\;${second}\\;=\\;?`)) :
+                (!powerFlip ?
+                    (`${first}\\;-\\;\\sqrt{${second}}\\;=\\;?`) :
+                    (`${first}\\;-\\;${second}^{2}\\;=\\;?`));
+        }
+        else if (main == 1) {
+            answerStr = mp;
+
+            exerciseStr = !turn ?
+                (!powerFlip ?
+                    (`\\sqrt{\\sqrt{${first}}\\;-\\;${second}}\\;=\\;?`) :
+                    (`\\sqrt{${first}^{2}\\;-\\;${second}}\\;=\\;?`)) :
+                (!powerFlip ?
+                    (`\\sqrt{${first}\\;-\\;\\sqrt{${second}}}\\;=\\;?`) :
+                    (`\\sqrt{${first}\\;-\\;${second}^{2}}\\;=\\;?`));
+        }
+        else {
+            answerStr = mp ** 2;
+
+            exerciseStr = !turn ?
+                (!powerFlip ?
+                    (`(\\sqrt{${first}}\\;-\\;${second})^2\\;=\\;?`) :
+                    (`(${first}^{2}\\;-\\;${second})^2\\;=\\;?`)) :
+                (!powerFlip ?
+                    (`(${first}\\;-\\;\\sqrt{${second}})^2\\;=\\;?`) :
+                    (`(${first}\\;-\\;${second}^{2})^2\\;=\\;?`));
+        }
+
+        exercise.push({
+            type: 1, message: exerciseStr
+        });
+
+        answer.push(answerStr);
+    } //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DONE FIXED
+    else if (sign == 1) { // plus sign
+        mp = Math.floor(Math.random() * (quota - Math.floor(quota * 0.4))) + Math.floor(quota * 0.4);
+        mainPow = !main ? 0 : (main == 1 ? (mp ** 2) : (mp));
+        rawPow = !main ?
+            (Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1)) :
+            (Math.floor(Math.random() * ((mainPow - 1) - 1)) + 1);
+        first = !turn ?
+            (!powerFlip ? (rawPow ** 2) : (rawPow)) :
+            (!main ?
+                (Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1)) :
+                (mainPow - (!powerFlip ? (rawPow) : (rawPow ** 2))));
+        second = !!turn ?
+            (!powerFlip ? (rawPow ** 2) : (rawPow)) :
+            (!main ?
+                (Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1)) :
+                (mainPow - (!powerFlip ? (rawPow) : (rawPow ** 2))));
+
+        let exerciseStr;
+        let answerStr;
+
+        if (!main) {
+            answerStr = !turn ?
+                (!powerFlip ?
+                    (rawPow + second) :
+                    (rawPow ** 2 + second)) :
+                (!powerFlip ?
+                    (first + rawPow) :
+                    (first + rawPow ** 2));
+
+            exerciseStr = !turn ?
+                (!powerFlip ?
+                    (`\\sqrt{${first}}\\;+\\;${second}\\;=\\;?`) :
+                    (`${first}^{2}\\;+\\;${second}\\;=\\;?`)) :
+                (!powerFlip ?
+                    (`${first}\\;+\\;\\sqrt{${second}}\\;=\\;?`) :
+                    (`${first}\\;+\\;${second}^{2}\\;=\\;?`));
+        }
+        else if (main == 1) {
+            answerStr = mp;
+
+            exerciseStr = !turn ?
+                (!powerFlip ?
+                    (`\\sqrt{\\sqrt{${first}}\\;+\\;${second}}\\;=\\;?`) :
+                    (`\\sqrt{${first}^{2}\\;+\\;${second}}\\;=\\;?`)) :
+                (!powerFlip ?
+                    (`\\sqrt{${first}\\;+\\;\\sqrt{${second}}}\\;=\\;?`) :
+                    (`\\sqrt{${first}\\;+\\;${second}^{2}}\\;=\\;?`));
+        }
+        else {
+            answerStr = mp ** 2;
+
+            exerciseStr = !turn ?
+                (!powerFlip ?
+                    (`(\\sqrt{${first}}\\;+\\;${second})^2\\;=\\;?`) :
+                    (`(${first}^{2}\\;+\\;${second})^2\\;=\\;?`)) :
+                (!powerFlip ?
+                    (`(${first}\\;+\\;\\sqrt{${second}})^2\\;=\\;?`) :
+                    (`(${first}\\;+\\;${second}^{2})^2\\;=\\;?`));
+        }
+
+        exercise.push({
+            type: 1, message: exerciseStr
+        });
+
+        answer.push(answerStr);
+    } //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DONE FIXED
+    else if (sign == 2) { // divide sign
+        carrier = Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1);
+        rawPow = Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1);
+        first = !turn ?
+            (!powerFlip ? ((rawPow * carrier) ** 2) : (rawPow * carrier)) :
+            ((!powerFlip ? (rawPow) : (rawPow ** 2)) * carrier);
+        second = !!turn ? (!powerFlip ? (rawPow ** 2) : (rawPow)) : (carrier);
+
+        let answerStr = !turn ?
+            (!powerFlip ?
+                (Math.sqrt(first) / second) :
+                ((first) ** 2 / second)) :
+            (!powerFlip ?
+                (first / Math.sqrt(second)) :
+                (first / (second) ** 2));
+
+        let exerciseStr = !turn ?
+            (!powerFlip ?
+                (`\\sqrt{${first}}\\;:\\;${second}\\;=\\;?`) :
+                (`${first}^{2}\\;:\\;${second}\\;=\\;?`)) :
+            (!powerFlip ?
+                (`${first}\\;:\\;\\sqrt{${second}}\\;=\\;?`) :
+                (`${first}\\;:\\;${second}^{2}\\;=\\;?`));
+
+
+        exercise.push({
+            type: 1, message: exerciseStr
+        });
+
+        answer.push(answerStr);
+    } //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DONE
+    else if (sign == 3) { // multiply sign
+        rawPow = Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1);
+        first = !turn ?
+            (!powerFlip ? (rawPow ** 2) : (rawPow)) :
+            (Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1));
+        second = !!turn ?
+            (!powerFlip ? (rawPow ** 2) : (rawPow)) :
+            (Math.floor(Math.random() * (quota - Math.floor(quota * 0.1))) + Math.floor(quota * 0.1));
+
+        let answerStr = !turn ?
+            (!powerFlip ?
+                (rawPow * second) :
+                ((rawPow ** 2) * second)) :
+            (!powerFlip ?
+                (first * rawPow) :
+                (first * (rawPow ** 2)));
+
+        let exerciseStr = !turn ?
+            (!powerFlip ?
+                (`\\sqrt{${first}}\\;*\\;${second}\\;=\\;?`) :
+                (`${first}^{2}\\;*\\;${second}\\;=\\;?`)) :
+            (!powerFlip ?
+                (`${first}\\;*\\;\\sqrt{${second}}\\;=\\;?`) :
+                (`${first}\\;*\\;${second}^{2}\\;=\\;?`));
+
+
+        exercise.push({
+            type: 1, message: exerciseStr
+        });
+
+        answer.push(answerStr);
+    } //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DONE FIXED
+
+
+    box['exe'] = exercise;
+    box['ans'] = answer;
+    box['desc'] = false;
+
+    return box;
+}
+
+const powerAndRootTest = async (sett) => {
+    const pack = [];
+
+    const difficultyCheck = [30, 30, 30, 100, 100, 100, 100, 100, 100, 100];
+
+    difficultyCheck.forEach(async (diff) => {
+        const box = await powerAndRoot(diff, sett);
+
+        pack.push(box);
+    });
+
+    console.log(pack);
+
+    return pack;
+}
+
+const examBasic = async (sett) => {
+    console.log('tried to access exam');
+
+    const pack = [];
+
+    // must include forms and sizes
+    const mustHave = [
+        () => sumAndSub(100, sett),
+        () => multiplyAndDivide(100, sett),
+        () => mixed(100, sett),
+        () => fractions(70, sett),
+        () => powerAndRoot(100, sett),
+    ];
+    const additional = [
+        () => sumAndSub(100, sett),
+        () => multiplyAndDivide(100, sett),
+        () => mixed(100, sett),
+        () => fractions(70, sett),
+        () => powerAndRoot(100, sett),
+    ];
+
+    for (const thing of mustHave) {
+        const box = await thing();
+
+        pack.push(box);
+    }
+
+    while (pack.length < 10) {
+        const ind = Math.floor(Math.random() * additional.length);
+
+        const box = await additional[ind]();
+
+        pack.push(box);
+
+        additional.splice(ind, 1);
+    }
+
+    console.log(pack);
+
+    return pack;
+}
+
 const exercisePack = async (difficulty, topic, settings) => {
     let pack = [];
     let box = {};
@@ -678,6 +967,7 @@ const exercisePack = async (difficulty, topic, settings) => {
                         box = await mixed(difficulty, settings);
                         break;
                     case 'power_root':
+                        box = await powerAndRoot(difficulty, settings);
                         break;
                     case 'fraction_fractionMixed':
                         box = await fractions(difficulty, settings);
@@ -722,6 +1012,7 @@ const exercisePack = async (difficulty, topic, settings) => {
                     pack = await mixedTest(settings);
                     break;
                 case 'power_root':
+                    pack = await powerAndRootTest(settings);
                     break;
                 case 'fraction_fractionMixed':
                     pack = await fractionsTest(settings);
@@ -753,6 +1044,7 @@ const exercisePack = async (difficulty, topic, settings) => {
     else {
         switch (topic) {
             case 'exam_basic':
+                pack = await examBasic(settings);
                 break;
             case 'exam_advanced':
                 break;
